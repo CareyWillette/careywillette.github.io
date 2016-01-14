1a72be7a3cd8a1bf898e1e07513c74ceaa7d9c91
CareyWillette <Carey.Willette@gmail.com>
2016-01-13 22:11:55 -0700

    add anchor tag

--- controller.js
+++ controller.js
@@ -1,26 +1,8 @@
 angular.module('myWeb', [])
-    .controller('MainController', function($scope) {
-        
-    });
-    
-    .config(function($routeProvider, $locationProvider) {
-
-        $routeProvider
-            .when('/', {
-                templateUrl : 'partials/home.html',
-                controller : MainController
-            })
-            .when('/about', {
-                templateUrl : 'partials/about.html',
-                controller : MainController
-            })
-            .when('/contact', {
-                templateUrl : 'partials/contact.html',
-                controller : MainController
-            });
-    
-        // use the HTML5 History API
-        $locationProvider.html5Mode(true);
-    });
- 
- 
+    app.controller('MainCtrl', function($scope, $location, $anchorScroll) {
+        $scope.scrollTo = function(id) {
+        $location.hash(id);
+        console.log($location.hash());
+        $anchorScroll();
+      };
+});
