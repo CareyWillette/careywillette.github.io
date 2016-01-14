4b9dbb047db2fc4bcec117d67b4ed471cea1a2a7
CareyWillette <Carey.Willette@gmail.com>
2016-01-13 21:44:58 -0700

    Adding my existing site.

---
+++ controller.js
@@ -0,0 +1,23 @@
+angular.module('myWeb', [])
+    
+    .config(function($routeProvider, $locationProvider) {
+
+        $routeProvider
+            .when('/', {
+                templateUrl : 'partials/home.html',
+                controller : MainController
+            })
+            .when('/about', {
+                templateUrl : 'partials/about.html',
+                controller : MainController
+            })
+            .when('/contact', {
+                templateUrl : 'partials/contact.html',
+                controller : MainController
+            });
+    
+        // use the HTML5 History API
+        $locationProvider.html5Mode(true);
+    });
+ 
+ 
