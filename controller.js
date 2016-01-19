9b5506c44afd90920ef50d94c776c347cbd4d8c7
CareyWillette <Carey.Willette@gmail.com>
2016-01-19 01:03:30 -0700

    added router, routes, templateUrl, and additional controllers to controller.js. Created projects folder

--- controller.js
+++ controller.js
@@ -1,8 +1,25 @@
-angular.module('myWeb', [])
+angular.module('myWeb', ['ngRoute'])
     .controller('MainCtrl', function($scope, $location, $anchorScroll) {
-        $scope.scrollTo = function(id) {
+      $scope.scrollTo = function(id) {
         $location.hash(id);
         console.log($location.hash());
         $anchorScroll();
       };
-});
+    })
+    .controller('NodeDroneCtrl', function($scope, $location, $anchorScroll) {
+      $scope.scrollTo = function(id) {
+        $location.hash(id);
+        console.log($location.hash());
+        $anchorScroll();
+      };
+      $scope.testTime = new Date();
+    })
+  .config(function($routeProvider, $locationProvider) {
+    $routeProvider
+      .when('/nodedrone', {
+        templateUrl: '/projects/nodedrone.html',
+        controller: 'NodeDroneCtrl'
+      })
+      .otherwise('/')
+		$locationProvider.html5Mode(true);
+  })
