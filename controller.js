f34d9088c7b51d8d3eab3b3dc85d39c9f9145aa5
CareyWillette <Carey.Willette@gmail.com>
2016-01-20 01:40:51 -0700

    updated project link to github repo. removed ngroute.

--- controller.js
+++ controller.js
@@ -1,25 +1,8 @@
-angular.module('myWeb', ['ngRoute'])
+angular.module('myWeb', [])
     .controller('MainCtrl', function($scope, $location, $anchorScroll) {
       $scope.scrollTo = function(id) {
         $location.hash(id);
         console.log($location.hash());
         $anchorScroll();
-      };
-    })
-    .controller('NodeDroneCtrl', function($scope, $location, $anchorScroll) {
-      $scope.scrollTo = function(id) {
-        $location.hash(id);
-        console.log($location.hash());
-        $anchorScroll();
-      };
-      $scope.testTime = new Date();
-    })
-  .config(function($routeProvider, $locationProvider) {
-    $routeProvider
-      .when('/nodedrone', {
-        templateUrl: '/projects/nodedrone.html',
-        controller: 'NodeDroneCtrl'
-      })
-      .otherwise('/')
-		$locationProvider.html5Mode(true);
-  })
+      }
+    });
